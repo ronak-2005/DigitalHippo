7bae01591a23aab9adac1e6cdd0d3eb15b274b9e
ronak-2005 <155297945+ronak-2005@users.noreply.github.com>
2024-01-11 17:11:50 +0530

    modify

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,103 +1,94 @@
-"use client"
+// "use client"
 
-import { Button, buttonVariants } from "@/components/ui/button";
-import { Icons } from "@/components/icons";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { cn } from "@/lib/utils";
-import { ArrowRight, Loader2 } from 'lucide-react'
-import Link from "next/link";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator";
-// import { toast } from 'sonner'
-import { trpc } from '@/trpc/client'
+// import { Button, buttonVariants } from "@/components/ui/button";
+// import { Icons } from "@/components/icons";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+// import { cn } from "@/lib/utils";
+// import { ArrowRight, Loader2 } from 'lucide-react'
+// import Link from "next/link";
+// import { useForm } from "react-hook-form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { AuthCredentialsValidator, TAuthCredentialsValidator } from "@/lib/validators/account-credentials-validator";
+// // import { toast } from 'sonner'
+// import { trpc } from '@/trpc/client'
 
-const page = () => {
-
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<TAuthCredentialsValidator>({
-    resolver: zodResolver(AuthCredentialsValidator),
-  })
-
- 
-   
-
-  // const { mutate: signIn, isLoading } =
-  // trpc.auth.signIn.useMutation({
-  //   onSuccess: async () => {
-  //     toast.success('Signed in successfully')
+// const page = () => {
 
 
-  const { mutate , isLoading } =
-  trpc.auth.createPayloadUser.useMutation({})
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<TAuthCredentialsValidator>({
+//     resolver: zodResolver(AuthCredentialsValidator),
+//   })
 
-  const onSubmit = ({
-    email,
-    password,
-  }: TAuthCredentialsValidator) => {
-    mutate({ email, password })
-  }
+//   const { mutate , isLoading } =
+//   trpc.auth.createPayloadUser.useMutation({})
 
-  return (
-    <>
-      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col items-center space-y-2 text-center">
-            <Icons.logo className="h-20 w-20" />
-            <h1 className="font-bold text-2xl">Create Account</h1>
-            <Link
-              className={buttonVariants({
-                variant: "link",
-                className: "gap-1.5",
-              })}
-              href="/login"
-            >
-              Already have an account? Login
-              <ArrowRight className="h-4 w-4" />
-            </Link>
-          </div>
-          <div className="grid gap-6">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="grid gap-2">
-                <div className="grid py-2 gap-1">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    {...register("email")}
-                    className={cn({
-                      "focus-visible:ring-red-500": errors.email,
-                    })}
-                    placeholder="you@example.com"
-                  />
-                </div>
-                <div className="grid py-2 gap-1">
-                  <Label htmlFor="password">Password</Label>
-                  <Input
-                  {...register("password")}
-                  type = 'password'
-                    className={cn({
-                      "focus-visible:ring-red-500": errors.password,
-                    })}
-                    placeholder="password"
-                  />
-                </div>
-                <Button disabled={isLoading}>
-                  {isLoading && (
-                    <Loader2 className='mr-2 h-4 w-4 animate-spin' />
-                  )}
-                  Create Account
-                </Button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+//   const onSubmit = ({
+//     email,
+//     password,
+//   }: TAuthCredentialsValidator) => {
+//     mutate({ email, password })
+//   }
 
-export default page;
+//   return (
+//     <>
+//       <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+//         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+//           <div className="flex flex-col items-center space-y-2 text-center">
+//             <Icons.logo className="h-20 w-20" />
+//             <h1 className="font-bold text-2xl">Create Account</h1>
+//             <Link
+//               className={buttonVariants({
+//                 variant: "link",
+//                 className: "gap-1.5",
+//               })}
+//               href="/login"
+//             >
+//               Already have an account? Login
+//               <ArrowRight className="h-4 w-4" />
+//             </Link>
+//           </div>
+//           <div className="grid gap-6">
+//             <form onSubmit={handleSubmit(onSubmit)}>
+//               <div className="grid gap-2">
+//                 <div className="grid py-2 gap-1">
+//                   <Label htmlFor="email">Email</Label>
+//                   <Input
+//                     {...register("email")}
+//                     className={cn({
+//                       "focus-visible:ring-red-500": errors.email,
+//                     })}
+//                     placeholder="you@example.com"
+//                   />
+//                 </div>
+//                 <div className="grid py-2 gap-1">
+//                   <Label htmlFor="password">Password</Label>
+//                   <Input
+//                   {...register("password")}
+//                   type = 'password'
+//                     className={cn({
+//                       "focus-visible:ring-red-500": errors.password,
+//                     })}
+//                     placeholder="password"
+//                   />
+//                 </div>
+//                 <Button disabled={isLoading}>
+//                   {isLoading && (
+//                     <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+//                   )}
+//                   Create Account
+//                 </Button>
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default page;
